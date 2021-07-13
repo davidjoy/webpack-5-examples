e7aa8954ec6f1fe0d53727f16dadd6774b40e7bf
David Joy <djoy@edx.org>
2021-07-13 10:46:57 -0400

    08-loaders: update dependencies and tweak configs

--- 08-loaders/config/webpack.config.js
+++ 08-loaders/config/webpack.config.js
@@ -1,7 +1,7 @@
 
-const Merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const envConfig = process.env.NODE_ENV === 'production' ? require('./webpack.config.prod') : require('./webpack.config.dev');
 
 // We use this here at edX
-module.exports = Merge.smart(require('./webpack.config.common'), envConfig);
+module.exports = merge(require('./webpack.config.common'), envConfig);
